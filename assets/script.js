7d80f372ed2b5b9dc475155f4b727026022172bf
João Giordani <jgiordanii@gmail.com>
2022-12-18 19:37:51 -0300

    version1.0

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,29 @@
 let length = document.querySelector('input#pass_length');
 let res = document.getElementById('res');
-let charsAll = "0123456789abcdefghijklmnopqrstuvwxyz.'!@#$%^&*()\"{}ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let charsNonSpecial = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz.'!@#$%^&*()\"{}ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let password = '';
-let check = document.querySelector('input[name="special"]:checked');
+var copyText = document.getElementById("password");
 
 function checkLength() {
     if (length.value.length == '') {
         window.alert('You must specify how many characters you desire.')
     } else {
         length = length.value
-        res.innerHTML = `The password length is ${length}.`
-    }
-}
-
-function checkSpecial() {
-    if (document.querySelectorAll('input[type="radio"]:checked').length === 0) {
-        window.alert("Not checked");
     }
 }
 
 function generate() {
     checkLength();
-    checkSpecial();
     res.innerHTML = '' 
     
     for (let c = 0; c < length; c++) {
-    let randomic = Math.floor(Math.random() * charsAll.length);
-    password += charsAll.substring(randomic, randomic + 1);
+    let randomic = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomic, randomic + 1);
     }
-    res.innerHTML += `The generated password is ${password}`
-    console.log(check)
+    document.getElementById("password").value = password;
 }
-        
+
+function copy() {
+    copyText.select();
+    document.execCommand("copy");
+}
